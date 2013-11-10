36f41b52a933d07aae4ede9381afab9ab8f6395e
gdi2290 <github@gdi2290.com>
2013-11-09 21:21:34 -0800

    refactor config.js

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,39 +1,30 @@
 'use strict';
 
-angular.module('easyApp')
-  .config(function($stateProvider, $urlRouterProvider, $locationProvider, RestangularProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'views/main.html'
-      })
-  })
+app.config(function($stateProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/main.html'
+    })
+});
 
-  .config(function($locationProvider) {
-    $locationProvider.html5Mode(true);
-  })
+app.config(function($locationProvider) {
+  $locationProvider.html5Mode(true);
+});
 
-  .config(function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('/1');
-    RestangularProvider.setDefaultHttpFields({
-      cache: false,
-      withCredentials: true
-    });
-  })
+app.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('/api');
+  RestangularProvider.setDefaultHttpFields({});
+});
 
-  .config(function(AngularyticsProvider) {
-    AngularyticsProvider.setEventHandlers(['Console', 'Google']);
-  })
-  .run(function(Angularytics) {
-    Angularytics.init();
-  })
+app.run(function(Angularytics) {
+  Angularytics.init();
+})
 
-  .run(function($rootScope, $location, $anchorScroll, $stateParams, $log) {
-    $rootScope.$log = $log;
-    $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-      $location.hash($stateParams.scrollTo);
-      $anchorScroll();
-    });
-  });
+app.run(function($rootScope, $state, $stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+
+});
 
 
