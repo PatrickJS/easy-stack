7ba1b191d5581509f5b0134c8e7fffb5529d62e2
gdi2290 <github@gdi2290.com>
2013-11-09 22:37:52 -0800

    update routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,15 @@
-var clouds = require('../controllers/clouds');
-
 module.exports = function(app) {
   app.get('/*', function(req, res) {
     res.render('index.html', {
-      title: 'EASY Stack'
+      title: 'EASY Stack',
+      description: '',
+      keywords: '',
+      author: ''
     });
   });
+};
+
+exports.template = function (req, res) {
+  var name = req.params.name;
+  res.render('template/' + name);
 };
