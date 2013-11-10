e66f8bea96c0fef3287ed2b0e24b2c5fd024dd01
gdi2290 <github@gdi2290.com>
2013-11-09 21:21:06 -0800

    refactor default filter

--- public/scripts/filters/default.js
+++ public/scripts/filters/default.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('easy.filters.default', [])
-  .filter('default', function() {
-    return function(text) {
-      return text;
-    }
-  });
+
+filters('default', function() {
+  return function(text) {
+    return text;
+  }
+});
